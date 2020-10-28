8c33421bbf8c5324ad17d8edf294a9a6edd8d1a2
mikeschaap <mike@mikeschaaps-MacBook-Pro.local>
2020-10-28 15:39:38 +0100

    Use embbeded browser engine and compile for windows

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/leaanthony/mewn v0.10.7
+	github.com/lxn/win v0.0.0-20201021184640-9c48443b1f44 // indirect
+	github.com/sciter-sdk/go-sciter v0.5.0
 	github.com/wailsapp/wails v1.7.1
 )
--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"github.com/leaanthony/mewn"
-	"github.com/wailsapp/wails"
+	"log"
+	"path/filepath"
+
+	"github.com/sciter-sdk/go-sciter"
+	"github.com/sciter-sdk/go-sciter/window"
 )
 
 func basic() string {
 	return "World!"
 }
 
+func main() {
+	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath, err := filepath.Abs("simple.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.LoadFile(fullpath)
+	w.SetTitle("Example")
+	w.Show()
+	w.Run()
+}
+
+/*
 func main() {
 
 	js := mewn.String("./frontend/dist/my-app/main.js")
@@ -25,3 +44,4 @@ func main() {
 	app.Bind(basic)
 	app.Run()
 }
+*/
